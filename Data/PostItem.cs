d626687806fb9205b6669d0a900edf33cfe86ac3
sho9029 <yatabe_9029@yahoo.co.jp>
2022-06-12 13:52:29 +0900

    Add Post.razor

---
+++ Data/PostItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCU_FleaMarket.Data;
+
+public class PostItem
+{
+    [Required]
+    public string? Name;
+    public string? Description;
+    public byte[]? Image;
+}
