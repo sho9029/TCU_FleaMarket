d11db45fdb7856c3c3eccf8e9ca8d54eede8b946
sho9029 <yatabe_9029@yahoo.co.jp>
2022-06-12 21:26:01 +0900

    Update Post.razor

--- wwwroot/js/post.js
+++ wwwroot/js/post.js
@@ -1,3 +1,23 @@
-﻿export async function setPrevImg(query, img) {
-    document.querySelector(query).src = img;
+﻿export function getImg() {
+    return document.querySelector("#prev-img").src;
+}
+
+export function setPrevImg() {
+    document.querySelector('#input-img').addEventListener('change', event => {
+        const file = event.target.files[0];
+        if (!file) {
+            return;
+        }
+        else if (!file.type.match('image/png') && !file.type.match('image/jpeg')) {
+            alert('Please select an image file.');
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = event => {
+            document.querySelector('#prev-img').src = event.target.result;
+        }
+
+        reader.readAsDataURL(file);
+    });
 }
